450888014c17e408a790385144b3ee98a45dc7bc
Herberson Miranda <herberson.miranda@outlook.com>
2023-08-24 23:00:33 -0300

    Refact: Refactory Tutor.java to TutorEntity.java

--- src/main/java/br/com/tutor/TutorEntity.java
+++ src/main/java/br/com/tutor/TutorEntity.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Tutor {
+public class TutorEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
