5e46073baf1e91350074e42abe6fbed7ded9da81
Herberson Miranda <herberson.miranda@outlook.com>
2023-08-30 16:58:20 -0300

    Refact: Refact code for create Abrigo Entity.

--- src/main/java/br/com/abrigo/AbrigoRepository.java
+++ src/main/java/br/com/abrigo/AbrigoRepository.java
@@ -2,7 +2,6 @@ package br.com.abrigo;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class AbrigoRepository implements PanacheRepository<AbrigoEntity> {
